e7542cf0fd5ef8ff757255c4e6a206b8274313bf
otronity <92176228+otronity@users.noreply.github.com>
2024-10-16 13:24:24 +0300

    Task
    Технічне завдання
    
    
    
    Розробіть систему для управління адресною книгою.
    
    
    
    Сутності:
    
    Field: Базовий клас для полів запису.
    Name: Клас для зберігання імені контакту. Обов'язкове поле.
    Phone: Клас для зберігання номера телефону. Має валідацію формату (10 цифр).
    Record: Клас для зберігання інформації про контакт, включаючи ім'я та список телефонів.
    AddressBook: Клас для зберігання та управління записами.
    
    
    Функціональність:
    
    AddressBook:Додавання записів.
    Пошук записів за іменем.
    Видалення записів за іменем.
    Record:Додавання телефонів.
    Видалення телефонів.
    Редагування телефонів.
    Пошук телефону.
    
    1. Клас AddressBook:
    
    Має наслідуватись від класу UserDict .
    Реалізовано метод add_record, який додає запис до self.data. Записи Record у AddressBook зберігаються як значення у словнику. В якості ключів використовується значення Record.name.value.
    Реалізовано метод find, який знаходить запис за ім'ям. На вхід отримує один аргумент - рядок, якій містить ім’я. Повертає об’єкт Record, або None, якщо запис не знайден.
    Реалізовано метод delete, який видаляє запис за ім'ям.
    Реалізовано магічний метод __str__ для красивого виводу об’єкту класу AddressBook .
    
    
    2. Клас Record:
    
    Реалізовано зберігання об'єкта Name в атрибуті name.
    Реалізовано зберігання списку об'єктів Phone в атрибуті phones.
    Реалізовано метод для додавання - add_phone .На вхід подається рядок, який містить номер телефона.
    Реалізовано метод для видалення - remove_phone. На вхід подається рядок, який містить номер телефона.
    Реалізовано метод для редагування - edit_phone. На вхід подається два аргумента - рядки, які містять старий номер телефона та новий. У разі некоректності вхідних даних метод має завершуватись помилкою ValueError.
    Реалізовано метод для пошуку об'єктів Phone - find_phone. На вхід подається рядок, який містить номер телефона. Метод має повертати або об’єкт Phone, або None .
    
    
    3. Клас Phone:
    
    Реалізовано валідацію номера телефону (має бути перевірка на 10 цифр).
    Наслідує клас Field. Значення зберігaється в полі value .

---
+++ main.py
@@ -0,0 +1,117 @@
+import re
+from collections import UserDict
+
+class Field:
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
+
+class Name(Field):
+    def __init__(self, name):
+        self.value = name
+
+class Phone(Field):
+    def add_phone(self, phone):
+        if bool(re.match(r'^\d{10}$', phone)):
+            self.value = phone
+        else:
+            raise ValueError('Incorrect Phone number')
+        
+class Record:
+    def __init__(self, name):
+        self.name = Name(name)
+        self.phones = []
+
+    def add_phone(self, phone):
+        try:
+            newphone = Phone(phone)
+            newphone.add_phone(phone)
+            # print(newphone)
+            self.phones.append(newphone)
+        except ValueError as e:
+            print(f"Failed to add phone: {str(e)}")
+
+    def edit_phone(self, phoneold, phonenew):
+        for phone in self.phones:
+            if phone.value == phoneold:
+                try:
+                    phone.add_phone(phonenew)
+                except ValueError as e:
+                    # Если новый номер некорректный, генерируем ошибку
+                    print(f"Failed to update phone: {str(e)}")
+                break
+
+    def find_phone(self, phone):
+        result = list(filter(lambda record: record.value == phone, self.phones))
+        return result[0] if len(result) > 0 else None
+    
+    def remove_phone(self, phone):
+        phonedel = self.find_phone(phone)
+        if phonedel:
+            self.phones.remove(phonedel)
+
+    def __str__(self):
+        return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
+
+class AddressBook(UserDict):
+    def add_record(self, record):        
+        self.data[record.name.value] = record
+
+    def find(self, name):
+        result = list(filter(lambda record: record.name.value == name, self.data.values()))
+        return result[0] if len(result) > 0 else None
+    
+    def delete(self, name):
+        record = self.find(name)
+        if record:
+            del self.data[record.name.value]
+
+    def __str__(self):
+        if not self.data:
+            return "Address Book is empty."
+
+        contacts_str = "\n".join([f"{record.name.value}: {', '.join(p.value for p in record.phones) if record.phones else 'No phones'}"
+                                 for record in self.data.values()])
+        return f"Address Book:\n{contacts_str}"
+        
+
+
+# Створення нової адресної книги
+book = AddressBook()
+
+# Створення запису для John
+john_record = Record("John")
+john_record.add_phone("1234567890")
+john_record.add_phone("5555555555")
+
+# Додавання запису John до адресної книги
+book.add_record(john_record)
+
+# Створення та додавання нового запису для Jane
+jane_record = Record("Jane")
+jane_record.add_phone("9876543210")
+book.add_record(jane_record)
+
+# Виведення всіх записів у книзі
+print(book)
+
+# Знаходження та редагування телефону для John
+john = book.find("John")
+john.edit_phone("1234567890", "1112223333")
+
+print(john)  # Виведення: Contact name: John, phones: 1112223333; 5555555555
+
+# Пошук конкретного телефону у записі John
+found_phone = john.find_phone("5555555555")
+print(f"{john.name}: {found_phone}")  # Виведення: John: 5555555555
+
+# Видалення запису Jane
+book.delete("Jane")
+print(book)
+
+book.find("John").edit_phone('5555555555', '1111111') # Failed to update phone: Incorrect Phone number
+john.add_phone("111") # Failed to add phone: Incorrect Phone number
+john.remove_phone('5555555555')
+print(john)
